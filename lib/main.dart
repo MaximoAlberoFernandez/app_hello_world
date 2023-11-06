c17b9573c28fa26f09d7718bd45ebfe8af4665e7
MaximoAlberoFernandez <maximo.alberofernandez@gmail.com>
2023-11-06 12:45:25 +0100

    Changes on display

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("Hello, World!")
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Row(
-            children:[ 
-              Text("Máximo Albero - Curs 23/24")
-            ]
-          )
-        ],)
-    )
-  ));
+      home: Scaffold(
+          appBar: AppBar(title: Text("Hello, World!"),
+          backgroundColor: Color.fromARGB(255, 26, 152, 255),
+          ),
+          body: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                  height: 10,
+              ),
+              Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Máximo Albero - Curs 23/24")
+              ])
+            ],
+          ))));
 }
